6915d7728bcfa73fa561b9a5f57a77c2a3d1f285
Paul Rumyantsev <p.rumyantsev@itransition.com>
2018-08-07 14:05:38 +0300

    Update storybook

---
+++ docs/dist/src/Footer/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import './style.css';
+export declare const Footer: () => JSX.Element;
+export default Footer;
---
+++ docs/dist/src/Social/index.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react" />
+import './icons.css';
+import './style.css';
+export interface SocialProps {
+    theme: any;
+}
+export declare const Social: ({ theme }: SocialProps) => JSX.Element;
+export default Social;
