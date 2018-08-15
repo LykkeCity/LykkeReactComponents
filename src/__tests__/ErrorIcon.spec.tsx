8ffa94440416c6e5f62214a939c9b77bdf73108d
George Moiseychik <igeorg3i@gmail.com>
2018-08-15 14:15:51 +0300

    add-error-page-improvements: updated tests

--- src/__tests__/ErrorIcon.spec.tsx
+++ src/__tests__/ErrorIcon.spec.tsx
@@ -5,19 +5,19 @@ import {ErrorIcon} from '../AppError';
 
 Enzyme.configure({adapter: new Adapter()});
 
-test('Application Error section should be div by default', () => {
+test('Error Icon should be div by default', () => {
   const errorIcon = shallow(<ErrorIcon />);
   expect(errorIcon.closest('div')).toHaveLength(1);
 });
 
-test('Application Error should set reload button text', () => {
+test('Error Icon should set background color', () => {
   const errorIcon = mount(<ErrorIcon backgroundColor="#fff" />);
   expect(errorIcon.props().backgroundColor).toEqual('#fff');
   errorIcon.setProps({backgroundColor: '#000'});
   expect(errorIcon.props().backgroundColor).toEqual('#000');
 });
 
-test('Application Error should set reload button text', () => {
+test('Error Icon should set color of the icon', () => {
   const errorIcon = mount(<ErrorIcon signColor="red" />);
   expect(errorIcon.props().signColor).toEqual('red');
 });
