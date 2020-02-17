32196519a8058b08f31563d6b65f45270e342bba
Ozgur GUL <lugrugzo@gmail.com>
2020-02-17 11:18:49 +0300

    Changed appstore link.

--- docs/dist/src/Header/AppLinks.d.ts
+++ docs/dist/src/Header/AppLinks.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="react" />
 import React from 'react';
-export declare const APPSTORE_LINK = "https://appsto.re/ru/Dwjvcb.i";
+export declare const APPSTORE_LINK = "https://itunes.apple.com/de/app/id1112839581/";
 export declare const GOOGLEPLAY_LINK = "https://play.google.com/store/apps/details?id=com.lykkex.LykkeWallet";
 export interface AppLinksProps {
     className?: string;
--- src/Header/AppLinks.tsx
+++ src/Header/AppLinks.tsx
@@ -1,7 +1,7 @@
 import classnames from 'classnames';
 import React from 'react';
 
-export const APPSTORE_LINK = 'https://appsto.re/ru/Dwjvcb.i';
+export const APPSTORE_LINK = 'https://itunes.apple.com/de/app/id1112839581/';
 export const GOOGLEPLAY_LINK =
   'https://play.google.com/store/apps/details?id=com.lykkex.LykkeWallet';
 
